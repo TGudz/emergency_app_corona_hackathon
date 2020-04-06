dbd90c1cb56a2f72476b693e84ff37f7d4f26431
Taras Gudz <taras.mail.main@gmail.com>
2020-04-06 18:45:33 +0300

    add basic reactivity

--- src/app/home/table/table.component.ts
+++ src/app/home/table/table.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+class OrderDetail {
+  constructor(
+     public name: string,
+     public score: number,
+     public amount: number,
+     public checked: boolean = true,
+  ) { }
+};
+
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -7,8 +17,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  public orderDetails: OrderDetail[] = [
+    new OrderDetail('Faces&Names', 100, 100),
+    new OrderDetail('Friends', 98, 200),
+    new OrderDetail('No Name', 95, 50),
+    new OrderDetail('Carmen', 94, 500, false),
+    new OrderDetail('BFS', 94, 500, false),
+    new OrderDetail('Euler\'s', 91, 1000, false),
+  ];
+
   constructor() { }
 
   ngOnInit() {}
 
+  getTotalOrders() {
+    let total = 0;
+    this.orderDetails.forEach(item => {
+      if (item.checked) {
+        total += item.amount;
+      }});
+    return total;
+  }
 }
